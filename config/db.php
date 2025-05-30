596cd1a3842e8d9b88a62d1647a1d2a06558c44e
type-wd-17298 <120540900+type-wd-17298@users.noreply.github.com>
2025-05-30 12:01:36 +0700

    change db as before

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=hms_product_db',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.200.9;dbname=hms_product_db',
+    'username' => 'ptaung',
+    'password' => 'QW2267er##',
     'charset' => 'utf8',
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
